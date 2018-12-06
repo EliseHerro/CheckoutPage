8afc63851e2d8f777797ff5176afe332c8a97273
EliseConfig <eliseherro@hotmail.com>
2018-12-06 22:54:55 +0100

    Adding Stripe through NuGet Adding Stripe Views Charge Adding Charge Method to the HomeViewController Adding Stripe Keys to Webconfig Registering the Keys into Stripeconfiguration to setKeys into Stripe call header Pass the keys to the Stripe View

--- CheckoutPage/Controllers/HomeController.cs
+++ CheckoutPage/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Stripe;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,5 +22,46 @@ namespace CheckoutPage.Controllers
             return View(product);
         }
 
+
+        public ActionResult StripePay()
+        {
+
+            var stripePublishKey = ConfigurationManager.AppSettings["PublishableKey"];
+   
+   
+            ViewBag.StripePublishKey = "pk_test_PcbswzmLnVWD8jEzByVBWoCE";
+            return View();
+        }
+
+        public ActionResult Charge(string stripeEmail, string stripeToken)
+        {
+           
+            var customers = new CustomerService();
+            var charges = new ChargeService();
+
+            var customer = customers.Create(new CustomerCreateOptions
+            {
+                Email = stripeEmail,
+                SourceToken = stripeToken
+            });
+
+            var charge = charges.Create(new ChargeCreateOptions
+            {
+                Amount = 100,//cent
+                Description = "Sample Charge",
+                Currency = "usd",
+                CustomerId = customer.Id
+            });
+
+            
+
+            return View();
+        }
+
+
+        public ActionResult Error()
+        {
+            return View();
+        }
     }
 }
